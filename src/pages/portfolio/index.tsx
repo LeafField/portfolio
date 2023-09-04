6aa230eafe2d1382626deda6dcb2cf79601efe04
LeafField <109466326+LeafField@users.noreply.github.com>
2023-09-04 16:49:03 +0900

    portfolioページの移動

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PortfolioPage = () => {
-  return <div>PortfolioPage</div>;
+  return <div className=" h-96 bg-slate-500 pt-28">ポートフォリオページ</div>;
 };
 
 export default PortfolioPage;
