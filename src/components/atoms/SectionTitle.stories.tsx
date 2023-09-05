a04b63b9e5dba747319fe0e78f43d2a5ed642ec7
LeafField <109466326+LeafField@users.noreply.github.com>
2023-09-05 12:18:15 +0900

    セクションタイトルの作成

---
+++ src/components/atoms/SectionTitle.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SectionTitle from "./SectionTitle";
+
+const meta: Meta = {
+  title: "atom/SectionTitle",
+  component: SectionTitle,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Skills: Story = {
+  args: {
+    title: "Skills",
+  },
+};
+
+export const PortFolio: Story = {
+  args: {
+    title: "Portfolio",
+  },
+};
